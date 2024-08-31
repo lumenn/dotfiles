41e93f9b316e85b3bbe93ee0539a10494e44b674
Maksymilian Lumenn Stachowiak <silmelumenn@gmail.com>
2024-08-31 18:29:06 +0200

    Corrected path to keepassxc database.

--- .config/fish/functions/set-env-tokens.fish
+++ .config/fish/functions/set-env-tokens.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/fish
 function set-env-tokens
-  export OPENAI_API_KEY="$(keepassxc-cli show gdrive/KeepassXC/Secure.kdbx -a API_TOKEN 'Chat Gpt')"
+  export OPENAI_API_KEY="$(keepassxc-cli show ~/gdrive/KeepassXC/Secure.kdbx -a API_TOKEN 'Chat Gpt')"
 end
