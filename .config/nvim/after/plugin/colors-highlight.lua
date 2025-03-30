9edcf26761ab2bd40b15cc7a37d89b51e3816f73
Maksymilian Lumenn Stachowiak <silmelumenn@gmail.com>
2025-03-30 19:50:09 +0200

    Rebuilding neovim config, added which-key.

--- .config/nvim/after/plugin/colors-highlight.lua
+++
@@ -1,8 +0,0 @@
-require('nvim-highlight-colors').setup {
-    render = 'virtual',
-    --  #FFFFFF
-
-    virtual_symbol_prefix = ' ',
-    virtual_symbol = '',
-    virtual_symbol_position = 'eow'
-}
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,22 @@
-require('lumenn.remap')
-require('lumenn.lazy')
-require('lumenn.set')
+require("config.lazy")
 
--- Function to detect Hugo HTML template files
-local function detect_hugo_template()
-  local filename = vim.fn.expand('%:e')
-  local content = vim.fn.getline(1, '$')
-
-  if filename == "html" and vim.fn.search("{{") ~= 0 then
-    vim.bo.filetype = "gotmpl"
-  end
-end
-
--- Create an autocommand group for file detection
-vim.api.nvim_create_augroup("HugoTemplateDetection", { clear = true })
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-  pattern = "*",
-  callback = detect_hugo_template,
-  group = "HugoTemplateDetection",
-})
+vim.opt.copyindent = true
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.cursorline = true
+vim.opt.hlsearch = true
+vim.opt.list = true
+vim.opt.number = true
+vim.opt.numberwidth=4
+vim.opt.relativenumber = true
+vim.opt.preserveindent = true
+vim.opt.quoteescape = "\\"
+vim.opt.shiftwidth = 4
+vim.opt.showcmd = false
+vim.opt.wrap = false
+vim.opt.showbreak = "|=> "
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.smoothscroll = true
+vim.opt.softtabstop = 4
+vim.opt.virtualedit = all
