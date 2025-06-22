a7ae67c3d93bc30696268f2617a32aebbe090932
Maksymilian Lúmenn Stachowiak <silmelumenn@gmail.com>
2025-06-22 21:55:43 +0200

    Updated script to correctly set env variables.

--- .config/fish/functions/set-env-tokens.fish
+++ .config/fish/functions/set-env-tokens.fish
@@ -9,9 +9,11 @@ function set-env-tokens
   # Retrieve Anthropic API token
   set ANTHROPIC_API_KEY (echo $KEEPASS_PASSWORD | keepassxc-cli show ~/gdrive/KeepassXC/Secure.kdbx 'claude.ai' -a API_TOKEN)
 
+  echo $OPENAI_API_KEY
+  echo $ANTHROPIC_API_KEY
   # Export the API keys
-  export OPENAI_API_KEY
-  export ANTHROPIC_API_KEY
+  set -gx OPENAI_API_KEY $OPENAI_API_KEY
+  set -gx ANTHROPIC_API_KEY $ANTHROPIC_API_KEY
 
   # Clean up
   set -e KEEPASS_PASSWORD
